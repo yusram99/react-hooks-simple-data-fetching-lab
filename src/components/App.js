83bb06b7646d265803fae738ef8e23b3cf1f33cf
Your Name <youremail@example.com>
2023-08-02 15:59:30 +0300

    commit message

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React,{useState,useEffect} from "react";
+const App = () => {
+    
+    const[dogImage,SetDogImage]=useState(null)
+    const[isLoaded,SetisLoaded]=useState(false)
+    useEffect(() => {
+
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => {
+
+            SetDogImage(data.message)
+            SetisLoaded(true)
+        })
+    },[])
+return(
+
+    <div>
+            {isLoaded?(
+                <img src={dogImage} alt='A Random Dog'></img>
+            ):(
+                <p>Loading...</p>
+            )}
+            
+        </div>
+)
+
+
+}
+export default App;
